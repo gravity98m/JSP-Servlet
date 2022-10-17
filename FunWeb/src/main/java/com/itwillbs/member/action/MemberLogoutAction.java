31d23fd06ac177d60215d2d081b8c6d6a47e0cd5
itwill07 <blacksugars2s2@gmail.com>
2022-10-17 17:49:38 +0900

    회원로그아웃

---
+++ FunWeb/src/main/java/com/itwillbs/member/action/MemberLogoutAction.java
@@ -0,0 +1,26 @@
+package com.itwillbs.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class MemberLogoutAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println(" M : MemberLogoutAction_execute() 호출" );
+
+		// 로그아웃(세션정보 초기화)
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		// 정보 전달
+		ActionForward forward = new ActionForward();
+		forward.setPath("./Main.jsp");
+		forward.setRedirect(true);
+		
+		return forward;
+	}
+	
+	
+}
